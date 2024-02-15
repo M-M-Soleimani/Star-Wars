c210c449e30f8d528a0d7d620758e81d0d32aabe
Your Name <m.m.soleimani84@gmail.com>
2024-02-15 18:29:34 +0330

    Completing the data structure of the map components and adding the position function

--- StarWars.cpp
+++ StarWars.cpp
@@ -3,6 +3,8 @@
 #include <windows.h>
 #include<vector>
 #include<string>
+#include <time.h>
+#include <stdlib.h>
 
 // Defining color codes to beautify the game
 #define Black "\u001b[30m"
@@ -25,18 +27,22 @@
 
 using namespace std;
 
-struct Map_Components   // A data structure for the components in the game map
+struct Map_Components   // A data structure for the components in the game map with Initialization
 {
-    string name ;
-    int Health ;
-    int damage ;
+    string name = "empty";
+    int Health = 0;
+    int damage = 0 ;
+    int size = 1*1 ;
+    string color ;
+    string character = "[ ]";
 };
 
 
 bool Menu();    // function that displays the menu to the user
 bool Game_Mode();   // function that allows the user to choose between two types of games
-void Initializer_Basic(); // A function for game preparation and initialization for Basic Mode
-void Initializer_Advanced(); // A function for game preparation and initialization for Advanced Mode
+void Initializer_Basic(); // function for game preparation and initialization for Basic Mode
+void Initializer_Advanced(); // function for game preparation and initialization for Advanced Mode
+void positioning( vector <vector<Map_Components>> map , int map_size ); // function that determines the position of elements in the map
 
 int main()
 {
@@ -178,6 +184,7 @@ void Initializer_Basic()
     cout << "Enter the quorum for the win : ";  // In these few lines, a quorum of points to win is received from the user
     int quorum_point;
     cin >> quorum_point;
+    positioning( map , map_size );  // This function specifies the position of game elements
 
 }
 
@@ -245,4 +252,116 @@ void Initializer_Advanced()
     } while (Invalid_Selection);
     cout << "Enter the map size : " << map_size << endl ;
     cout << "Enter the quorum for the win : " << quorum_point << endl; 
+}
+
+void positioning( vector <vector<Map_Components>> map , int map_size )
+{
+    srand(time(0)); // A function that generates random numbers with an initial seed of time 0
+    unsigned int row , column ;
+    for (size_t i = 0; i < (map_size - 1); i++) // In these few lines, if there is no native ship, a ship will be created
+    {
+        if (map[map_size - 1][i].name != "Spaceship")
+        {
+            map[map_size - 1][(map_size - 1) / 2].name = "Spaceship" ;
+            map[map_size - 1][(map_size - 1) / 2].Health = 3 ;
+            map[map_size - 1][(map_size - 1) / 2].damage = 2147483647 ; // The largest possible value for an integer data type
+            map[map_size - 1][(map_size - 1) / 2].size = 1 ;
+            map[map_size - 1][(map_size - 1) / 2].color = "Bright_Green" ;
+            map[map_size - 1][(map_size - 1) / 2].character = "[#]" ;
+        break;
+        }
+    }
+    int Enemy_Type ;
+    bool Invalid_position = false ;
+    Enemy_Type = rand() % 4 ;   // This function generates random numbers between 0 and 3
+    switch (Enemy_Type)     // This section selects the type of enemy to create in the map
+    {
+    case 0:     // In each section of the switch case, a type of enemy is created
+        column = rand() % map_size ;
+        map[0][column].name = "Dart" ;
+        map[0][column].Health = 1 ;
+        map[0][column].damage = 1 ;
+        map[0][column].size = 1*1 ;
+        map[0][column].color = "Bright_Cyan" ;
+        map[0][column].character = "[*]" ;
+        break;
+    
+    case 1:
+        do
+        {
+        column = rand() % map_size ;
+        if (column + 2 <= map_size )
+        {
+            for (size_t i = 0; i < 2; i++)
+            {
+                for (size_t j = 0; j < 2; j++)
+                {
+                    map[i][column + j].name = "Striker" ;
+                    map[i][column + j].Health = 2 ;
+                    map[i][column + j].damage = 1 ;
+                    map[i][column + j].size = 2*2 ;
+                    map[i][column + j].color = "Bright_Blue" ;
+                    map[i][column + j].character = "[*]" ;
+                } 
+            }
+        }
+        else
+        {
+            Invalid_position = true ;
+        }
+        } while (Invalid_position);
+        break;
+    
+    case 2:
+        Invalid_position = false ;
+        do
+        {
+        if (column + 3 <= map_size)
+        {
+            for (size_t i = 0; i < 3; i++)
+            {
+                for (size_t j = 0; j < 3; j++)
+                {
+                    map[i][column + j].name = "Wraith" ;
+                    map[i][column + j].Health = 4 ;
+                    map[i][column + j].damage = 1 ;
+                    map[i][column + j].size = 3*3 ;
+                    map[i][column + j].color = "Blue" ;
+                    map[i][column + j].character = "[*]" ;
+                } 
+            }
+        }
+        else
+        {
+            Invalid_position = true ;
+        }
+        } while (Invalid_position);
+        break;
+
+    case 3:
+        Invalid_position = false ;
+        do
+        {
+        if (column + 4 <= map_size)
+        {
+            for (size_t i = 0; i < 4; i++)
+            {
+                for (size_t j = 0; j < 4; j++)
+                {
+                    map[i][column + j].name = "Banshee" ;
+                    map[i][column + j].Health = 6 ;
+                    map[i][column + j].damage = 1 ;
+                    map[i][column + j].size = 4*4 ;
+                    map[i][column + j].color = "Magenta" ;
+                    map[i][column + j].character = "[*]" ;
+                } 
+            }
+        }
+        else
+        {
+            Invalid_position = true ;
+        }
+        } while (Invalid_position);
+        break;
+    }
 }
